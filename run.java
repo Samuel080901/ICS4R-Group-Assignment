4cf6a77c8784da5b9eb6455e90b56a4ec2e3f9e7
Samuel <zhu5@ocdsb.ca>
2026-05-08 11:02:26 -0400

    edit in run class

--- run.java
+++ run.java
@@ -1,11 +1,25 @@
 
 /**
- * Write a description of class run here.
+ * Ask User name,password.Create object using that input.  Save that information using fileIO. If User Account (Name) Already Exists,check if the password is saved in a txt file matching the password the user is inputting. 
+See if previous data exist or not,if exist using those data inside,recreate the object and save that into ArrayList
+Ask the user what they want to purchase,and create object using those information
+After all the items are entered,save those information into 1 text file. 
+In the end,ask if the user wants to purchase the things in the shopping cart or not. If yes delete all the txt file that contains item user wanted to purchase
+
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Samuel Zhu)
+ * @version (2026/05/08)
  */
 public class run
 {
+    //Samuel : create a 2D Array that will be used to output category and item in each category
+    String[][] aItemList = {
+        
+        {"Food","Furniture","Clothes","Electronic Device"},
+        {},
+        {},
+        {}
+    
+    };
     
 }
