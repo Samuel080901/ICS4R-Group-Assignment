2e2e943443c0b8187c919aba076c802a41a77bdc
Samuel <zhu5@ocdsb.ca>
2026-05-11 10:19:06 -0400

    update

--- run.java
+++ run.java
@@ -10,7 +10,7 @@ In the end,ask if the user wants to purchase the things in the shopping cart or 
  * @author (Samuel Zhu)
  * @version (2026/05/08)
  */
-
+//S: if there is comment with no name with it,its probably mine
 public class run
 {
     //Samuel : create a 2D Array that will be used to output category and item in each category
@@ -24,28 +24,67 @@ public class run
     
     };
     
-    //Samuel - Create 2 byte that store index for category and item
-    byte bytIndexC;
-    byte bytIndexI;
+    // S:create a 2D array that will be storing price of each item,use it when create the object
+    //the price is temporary
+    float[][] aItemPrice = {
+        
+        {0.01f,0.02f,0.03f,0.04f},
+        {0.05f,0.06f,0.07f},
+        {0.08f,0.09f,0.1f},
+        {0.11f,0.12f,0.13f},
+        {0.14f,0.15f,0.16f}
+    
+    };
     
     //Samuel - run method
     public void Run(){
-        //Samuel : output the list of category
-        System.out.println("Please Enter Cateogry of the item you want:\n");
-        for(byte i = 0; i < aItemList[0].length;i++){
-            System.out.println((i+1)+") " + aItemList[0][i] + "\n");
-        }
+        //Samuel - Create 2 byte that store index for category and item,intialize to -1 because we need to
+        byte bytIndexC = -1;
+        byte bytIndexI = -1;
+        
+        //S: create a byte value that store amount of item user wants to purchase
+        byte bytItemAmount;
+        
+        //S: create 3 string that will store extra info enter by user
+        //S: info saved will change base on item selected
+        String strTemp1;
+        String strTemp2;
+        String strTemp3;
+        
+        //S:create a do while loop that loop until user enter correct value
+        do{
+            //Samuel : output the list of category
+            System.out.println("Please Enter Cateogry of the item you want:\n");
+            for(byte i = 0; i < aItemList[0].length;i++){
+                System.out.println((i+1)+") " + aItemList[0][i] + "\n");
+            }
+            
+            
+            //Samuel : make a try catch
+            //S: ask for input
+            try{
+            bytIndexC = new Scanner(System.in).nextByte();
+            }
+            catch (Exception e){
+                
+                bytIndexC = 100;
+            }
+            //S: code a if statement to output user enter something wrong
+            if(bytIndexC <= 0 || bytIndexC > aItemList[0].length){
+                //S: clear terminal window
+               
+                System.out.println("Wrong input");
+                
+            }
+        }while(bytIndexC <= 0 || bytIndexC > aItemList[0].length);
         
         
-        //Samuel : make a try catch
-        //S: ask for input
-        try{
-        bytIndexC = new Scanner(System.in).nextByte();
-        }
-        catch (Exception e){
-            Run();
-        }
-        //Samuel : output the list of item in category that user chooses
+        
+        
+        
+        //S:create a do while loop that loop until user enter correct value
+        do{
+           //Samuel : output the list of item in category that user chooses
         System.out.println("Please Enter item you want:\n");
         for(byte i = 0; i < aItemList[bytIndexC].length;i++){
             System.out.println((i+1)+") " + aItemList[bytIndexC][i] + "\n");
@@ -54,22 +93,92 @@ public class run
         //Samuel : make a try catch
         //S: ask for input
         try{
-        bytIndexI = new Scanner(System.in).nextByte();
+            bytIndexI = new Scanner(System.in).nextByte();
+            
+            
         }
         catch (Exception e){
-            Run();
+            
+            bytIndexI = 100;
+        }
+        //S: code a if statement to output user enter something wrong
+        if(bytIndexI <= 0 || bytIndexI > aItemList[bytIndexC].length){
+            //S: clear terminal window
+            
+            System.out.println("Wrong input");
+            
+        }
+        }while(bytIndexI <= 0 || bytIndexI > aItemList[bytIndexC].length);
+        
+        
+        
+        //S:create a do while loop that loop until user enter correct value
+        do{
+               //S: output a message to ask user amount user wants to purchase
+            System.out.println("Please Enter Amount you want to purchase:\n");
+            //Samuel : make a try catch
+            //S: ask for input
+            try{
+                bytItemAmount = new Scanner(System.in).nextByte();
+                //S:output a message if user enter negative value
+                if(bytItemAmount <= 0){
+                    System.out.println("Value needs to be above 0");
+                    
+                }
+            }
+            catch (Exception e){
+                System.out.println("Wrong input");
+                bytItemAmount = -100;
+            }
+            
+        }while(bytItemAmount <= 0 );
+        
+        
+        
+        
+        
+        //S:ask user extra information based on what they choose to buy
+        //S: will be done using if statement
+        if(bytIndexC == 1){//S:(Food)
+            //S: for food we alse need to ask is it produce or not
+        
+        }
+        else if (bytIndexC == 2){//S:(Furniture)
+        
+        
+        }
+        else if (bytIndexC == 3){//S:(Clothes)
+        
+        
+        }
+        else if (bytIndexC == 4){//S:(Electronic Device)
+            //ask and populate the temp string
+            
+            
+        
         }
         
-        //S: output a message to ask user amount user wants to purchase
-        System.out.println("Please Enter Amount you want to purchase:\n");
-        //Samuel : make a try catch
-        //S: ask for input
-        try{
-        bytIndexI = new Scanner(System.in).nextByte();
-        }
-        catch (Exception e){
-            Run();
-        }
+        
+        
+        
+        
+        
+        
+    }
+    
+    //S:check is the number power of another power
+    //S: for example to check is 2048 power of 2,and return should be true
+    public boolean checkPower(float fltValue,short shrPower){
+        
+    
+    
+    
+    
+    
     }
         
 }
+
+
+
+
