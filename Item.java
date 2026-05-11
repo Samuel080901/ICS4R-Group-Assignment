048fcf9fd9bb43d4e20159c5fb2153f70f42a314
Samuel <zhu5@ocdsb.ca>
2026-05-11 10:20:41 -0400

    a

--- Item.java
+++ Item.java
@@ -9,18 +9,21 @@ public class Item
     //R instance variables
     String strName;
     float fltPrice; //for produce this is price per pound
+    byte bytItemAmount;
     
     //R default constructor
     public Item()
     {
        this.strName = "No name";
        this.fltPrice = 0.00f;
+       this.bytItemAmount = 0;
     }
     
     //R constructor
-    public Item(String n, float p)
+    public Item(String n, float p,byte i)
     {
        this.strName = n;
        this.fltPrice = p;
+       this.bytItemAmount = i;
     }
 }
