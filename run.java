83755e41c9f45f30c67221877800db9b0a0514a8
Samuel <zhu5@ocdsb.ca>
2026-05-14 11:20:33 -0400

    update

--- run.java
+++ run.java
@@ -212,7 +212,7 @@ public class run
                 
                 //create a object using all the input and save that into arralist
 
-                itemList.add(new Produce(aItemList[bytIndexC][bytIndexI-1],aItemPrice[bytIndexC-1][bytIndexI-1],bytItemAmount,bolTemp,(byte)12,fltTemp));
+                itemList.add(new Produce(aItemList[bytIndexC][bytIndexI-1],aItemPrice[bytIndexC-1][bytIndexI-1],bytItemAmount,bolTemp,fltTemp));
 
 
             } 
